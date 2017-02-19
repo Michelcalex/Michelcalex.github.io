23a6bc2bdc4c06dcd83ad29cfbeb8c62b24daae1
Michelcalex <Michelcalex+github@gmail.com>
2017-02-18 22:38:39 -0500

    Created gitignore and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+let gulp = require('gulp');
+let sass = require ('gulp-sass');
+let browser = require('gulp-browser');
+
+gulp.task('default', ['html', 'css', 'js', 'assets']);
+
+gulp.task('html', function() {
+    return gulp.src('index.html')
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('css', function() {
+    return gulp.src('styles/style.scss')
+        .pipe(sass()) 
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('js', function() {
+    return gulp.src('js/app.js')
+        .pipe(browser.browserify()) 
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('assets', function() {
+    return gulp.src(['assets/*.ico', 'assets/*.jpg', 'assets/*.png'])
+        .pipe(gulp.dest('public/assets'));
+});
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch('*.html', ['html']);
+    gulp.watch('styles/*.scss', ['css']);
+    gulp.watch('styles/partials/*.scss', ['css']);
+    gulp.watch('*.js', ['js']);
+    gulp.watch('assets/**.*', ['assets']);
+});
